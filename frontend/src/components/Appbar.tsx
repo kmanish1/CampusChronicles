a0f93ef89bda753edae869d4b7f2a85a1234e788
kmanish1 <[kmanishreddy11@gmail.com]>
2024-07-26 16:55:46 +0530

    app bar changes

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -6,12 +6,13 @@ export const Appbar = () => {
         <Link to={'/blogs'} className="flex flex-col text-2xl justify-center cursor-pointer font-bold">
                 Campus Chronicles
         </Link>
-        <div className="">
-            <Link to={`/publish`}>
-                <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">New</button>
+        <div className="flex items-center justify-center">
+            <Link to={`/publish`} className="flex items-center justify-center space-x-1">
+                {/* <img src="/edit.png" height={30} width={30}></img> */}
+                <button type="button" className="mr-4 text-white bg-green-700 hover:bg-gray-600 focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 flex items-center justify-center"><img src="/edit.png" height={30} width={30} className="mx-2"></img>New</button>
             </Link>
 
-            <Avatar size={"big"} name="Manish" />
+            {/* <Avatar size={"big"} name="Manish" /> */}
         </div>
     </div>
 }
